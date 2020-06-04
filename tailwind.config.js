14e0a39d1959fdd313c2b7d41360c9651215b8a6
gegehprast <gegeh.prast@gmail.com>
2020-06-05 00:32:57 +0800

    add inter font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,23 @@ module.exports = {
     },
     theme: {
         extend: {
+            fontFamily: {
+                sans: [
+                    'Inter',
+                    '-apple-system',
+                    'BlinkMacSystemFont',
+                    '"Segoe UI"',
+                    'Roboto',
+                    '"Helvetica Neue"',
+                    'Arial',
+                    '"Noto Sans"',
+                    'sans-serif',
+                    '"Apple Color Emoji"',
+                    '"Segoe UI Emoji"',
+                    '"Segoe UI Symbol"',
+                    '"Noto Color Emoji"',
+                ],
+            },
         },
     },
     variants: {},
