5fbc06c2ed50f209cb72ae6968a2209518417993
gegehprast <gegeh.prast@gmail.com>
2020-06-06 03:06:29 +0800

    implement link

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,25 +1,45 @@
 import React from 'react'
+import Link from 'next/link'
 
 interface Props {
     title: string
     cover: string
     text: string
     tags: string[]
+    link?: { href: string, as: string }
 }
 
-const Card: React.FC<Props> = ({ title, text, cover, tags }) => {
+const Card: React.FC<Props> = ({ title, text, cover, tags, link }) => {
     return (
         <div className="flex w-full px-1 mb-3 w-100 md:w-1/2 lg:w-1/3">
             <div className="relative flex flex-col flex-grow w-full overflow-hidden rounded shadow-lg">
                 <div className="relative w-full h-210-px md:h-190-px lg:h-210-px">
-                    <img className="absolute object-cover w-full h-full" src={cover} alt="Sunset in the mountains" />
-                    <div className="glass-helper">
-                        <div className="glass"></div>
-                    </div>
+                    {link ?
+                        <Link href={link.href} as={link.as} >
+                            <a>
+                                <img className="absolute object-cover w-full h-full" src={cover} alt="Sunset in the mountains" />
+                                <div className="absolute object-cover w-full h-full bg-black opacity-25" />
+                            </a>
+                        </Link> :
+                        <>
+                            <img className="absolute object-cover w-full h-full" src={cover} alt="Sunset in the mountains" />
+                            <div className="absolute object-cover w-full h-full bg-black opacity-25" />
+                        </>
+                    }
+                    
+                    
                 </div>
 
                 <div className="flex-1 px-5 py-3">
-                    <div className="mb-2 text-base font-bold lg:text-lg">{title}</div>
+                    <div className="mb-2 text-base font-bold lg:text-lg">
+                        {link ? 
+                            <Link href={link.href} as={link.as} >
+                                <a>{title}</a>
+                            </Link> :
+                            <>{title}</>
+                        }
+                    </div>
+                    
                     <p className="text-sm text-gray-700">
                         {text}
                     </p>
