9454bb8f42bb5523fca1dc4579b162e39af8ff11
gegehprast <gegeh.prast@gmail.com>
2020-06-06 23:48:26 +0800

    pushRouterQueries util function

---
+++ utils/util.ts
@@ -0,0 +1,52 @@
+/* eslint-disable no-prototype-builtins */
+
+import { NextRouter } from 'next/dist/client/router'
+
+/**
+ * Check if something is undefined.
+ */
+const isUndefined = (variable: any) => {
+    return typeof variable === 'undefined'
+}
+
+/**
+ * Make url query parameter from json object.
+ */
+const objectToUrlQuery = (object: any, applyArray = true) => {
+    let param = ''
+    for (const key in object) {
+        if (object.hasOwnProperty(key)) {
+            let element = object[key]
+
+            if (Array.isArray(object[key])) {
+                element = applyArray ? element.join(',') : null
+            }
+
+            if (element && !isUndefined(element) && element != '') {
+                param = param == '' ? `?${key}=${element}` : `${param}&${key}=${element}`
+            }
+        }
+    }
+
+    return param
+}
+
+/**
+ * Push next router. 
+ * This will only change the necessary query parameters of current route.
+ */
+export const pushRouterQueries = (router: NextRouter, { params, as = null, resetScroll = false }: pushRouterQueriesOptions): void => {
+    const newParams = { ...router.query, ...params }
+    const asPath = (as ? as : router.pathname) + objectToUrlQuery(newParams, false)
+
+    router.push({
+        pathname: router.pathname,
+        query: newParams,
+    }, asPath, { shallow: true }).then(() => resetScroll && window.scrollTo(0, 0))
+}
+
+interface pushRouterQueriesOptions {
+    params: any
+    as?: string | null | undefined
+    resetScroll?: boolean
+}
