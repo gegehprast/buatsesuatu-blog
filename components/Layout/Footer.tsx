4eabfc32b0f78804a734260b69619ce27014021a
gegehprast <gegeh.prast@gmail.com>
2020-06-07 01:30:34 +0800

    make footer always in bottom so it scale nicely

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer: React.FC = () => {
     return (
         <>
-            <div className="sticky top-0 z-50 w-full mt-10 bg-indigo-900 h-480-px">
+            <div className="z-50 w-full mt-10 bg-indigo-900 h-480-px">
                 
             </div>
         </>
