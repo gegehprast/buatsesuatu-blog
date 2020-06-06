9265342662a3ecd9cdc6eccb730bc7467aad4859
gegehprast <gegeh.prast@gmail.com>
2020-06-06 23:48:12 +0800

    add default activePage 1

--- pages/api/articles.ts
+++ pages/api/articles.ts
@@ -5,7 +5,7 @@ type Index = (req: NextApiRequest, res: NextApiResponse) => void
 
 const index: Index = (req: NextApiRequest, res: NextApiResponse) => {
     const perPage = 6
-    const activePage = parseInt(req.query.page as string)
+    const activePage = req.query.page ? parseInt(req.query.page as string) : 1
     const total = articles.length
     const from = (activePage - 1) * perPage
     const to = from + perPage
