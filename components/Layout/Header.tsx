ca189a5c03c091c817150599a9d600c16f1c7fb5
gegehprast <gegeh.prast@gmail.com>
2020-06-08 01:18:05 +0800

    home link

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -13,7 +13,11 @@ const Header: React.FC = () => {
                 
                 <div className="flex flex-wrap items-center justify-center w-full px-3 py-2 mx-auto md:justify-between md:w-3/4 lg:w-5/6 xl:w-1/2 xxl-1344:w-4/6 xxl-1920:w-1/2 xxl-4k:w-1/3">
                     <div className="w-full py-1 mt-1 text-center lg:mt-0 md:w-auto md:text-left">
-                        <h2 className="text-xl font-bold leading-none lg:text-2xl">Oof!</h2>
+                        <Link href="/" as="/">
+                            <a>
+                                <h2 className="text-xl font-bold leading-none lg:text-2xl">Oof!</h2>
+                            </a>
+                        </Link>
                     </div>
 
                     <div className="flex py-1 mt-1 w-100 lg:mt-0">
