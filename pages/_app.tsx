7088050ab82c736a0f17558d9dbee3a056f4098e
gegehprast <gegeh.prast@gmail.com>
2020-06-09 02:38:10 +0800

    reactPlaceholder style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { AppProps } from 'next/app'
 import { NextComponentType } from 'next'
 import 'easymde/dist/easymde.min.css'
 import 'github-markdown-css/github-markdown.css'
+import 'react-placeholder/lib/reactPlaceholder.css'
 import '../styles/inter.css'
 import '../styles/main.css'
 
