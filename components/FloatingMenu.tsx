50fb141d3cb09c915406d9b779f64096af3494b0
gegehprast <gegeh.prast@gmail.com>
2020-06-09 00:12:13 +0800

    menu z index

--- components/FloatingMenu.tsx
+++ components/FloatingMenu.tsx
@@ -20,7 +20,7 @@ const FloatingMenu = (): React.ReactElement | null => {
     }
 
     return (
-        <div className="fixed bottom-0 right-0 w-full appearance-none md:px-4 md:w-2/6 lg:w-1/4">
+        <div className="fixed bottom-0 right-0 z-10 w-full appearance-none md:px-4 md:w-2/6 lg:w-1/4">
             <div className="p-2 text-gray-900 bg-gray-100 border-t shadow border-t-gray-500 md:border md:border-gray-600 md:rounded">
                 <div className="font-semibold leading-none">
                     Hello, {user.name}!
