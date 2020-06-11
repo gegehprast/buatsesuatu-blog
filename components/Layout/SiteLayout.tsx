74adb992f32935bbbc5be582a60d29dd05fa367f
gegehprast <gegeh.prast@gmail.com>
2020-06-11 22:32:19 +0800

    add meta tags

--- components/Layout/SiteLayout.tsx
+++ components/Layout/SiteLayout.tsx
@@ -27,9 +27,15 @@ const SiteLayout = ({ children }: Props): JSX.Element => {
     return (
         <>
             <Head>
-                <title key="title">Buat Sesuatu</title>
+                <title key="title">{process.env.NEXT_PUBLIC_TITLE}</title>
                 <meta charSet="utf-8" />
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+                <meta key="description" name="description" content={process.env.NEXT_PUBLIC_DESCRIPTION} />
+                <meta key="keywords" name="keywords" content={process.env.NEXT_PUBLIC_KEYWORDS} />
+                <meta key="author" name="author" content={process.env.NEXT_PUBLIC_AUTHOR} />
+                <meta name="google-site-verification" content={process.env.NEXT_PUBLIC_ANALYTIC} />
+
                 <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
                 <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
                 <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
@@ -46,6 +52,17 @@ const SiteLayout = ({ children }: Props): JSX.Element => {
                 <link rel="manifest" href="/manifest.json" />
                 <meta name="msapplication-TileColor" content="#ffffff" />
                 <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+
+                <meta key="og:site_name" property="og:site_name" content={process.env.NEXT_PUBLIC_OG_SITE_NAME} />
+                <meta key="og:type" property="og:type" content="website" />
+                <meta key="og:image" property="og:image" content={process.env.NEXT_PUBLIC_OG_IMAGE} />
+                <meta key="og:image:alt" property="og:image:alt" content={process.env.NEXT_PUBLIC_OG_IMAGE_ALT} />
+                <meta key="og:title" property="og:title" content={process.env.NEXT_PUBLIC_OG_TITLE} />
+                <meta key="og:description" property="og:description" content={process.env.NEXT_PUBLIC_OG_DESCRIPTION} />
+                <meta key="og:url" property="og:url" content={process.env.NEXT_PUBLIC_OG_URL} />
+
+                <link key="canonical" rel="canonical" href={process.env.NEXT_PUBLIC_CANONICAL} />
+                <link rel="search" type="application/opensearchdescription+xml" title={process.env.NEXT_PUBLIC_TITLE} href="/opensearch.xml" />
             </Head>
 
             <div className={`${darkMode && 'dark-mode'}`}>
