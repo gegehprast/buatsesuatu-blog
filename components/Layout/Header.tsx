4ee4274273d2740b180f96e87a713f450e9ceb1b
gegehprast <gegeh.prast@gmail.com>
2020-06-11 21:31:28 +0800

    fix title, search bar, and dark button

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -40,12 +40,12 @@ const Header = ({ darkMode, setDarkMode }: { darkMode: boolean, setDarkMode: () 
                     <div className="w-full py-1 mt-1 text-center lg:mt-0 md:w-auto md:text-left">
                         <Link href="/" as="/">
                             <a>
-                                <h2 className="text-xl font-bold leading-none lg:text-2xl">Oof!</h2>
+                                <h2 className="text-xl font-bold leading-none lg:text-2xl">Buat Sesuatu</h2>
                             </a>
                         </Link>
                     </div>
 
-                    <div className="flex self-stretch w-full mt-3 mb-2 mr-2 md:w-1/2 md:mb-0 md:mt-1 md:mx-4 lg:mt-0">
+                    <div className="flex w-1/2 mt-3 mb-2 mr-2 lg:self-stretch md:mb-0 md:mt-1 md:mx-4 lg:mt-0">
                         <input type="text" 
                             className="w-full px-3 py-1 border-b-4 border-b-gray-300 focus:outline-none focus:border-b-gray-900" 
                             onChange={(e) => setSearch(e.target.value)} 
@@ -55,7 +55,7 @@ const Header = ({ darkMode, setDarkMode }: { darkMode: boolean, setDarkMode: () 
                         />
                     </div>
 
-                    <div className="flex items-center py-1 mt-3 mb-2 md:mb-0 md:mt-1 lg:mt-0">
+                    <div className="flex items-center py-1 mt-3 md:mb-0 md:mt-1 lg:mt-0">
                         <div className="flex items-end ml-8">
                             <button className={`darkModeButton ${darkMode ? 'text-white' : 'text-gray-900'}`} onClick={setDarkMode} title="Mode malam">
                                 <svg className="w-6 h-6 leading-none fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
