06a16d8c6793eafdf19b0d6ee4592a7d64ed07c6
gegehprast <gegeh.prast@gmail.com>
2020-06-12 22:54:10 +0800

    tag bg if match withe route tags query

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/dist/client/router'
 
 interface Props {
     length: number
@@ -8,9 +9,13 @@ interface Props {
 }
 
 const Tag = ({ length, tag, i }: Props): JSX.Element => {
+    const router = useRouter()
+
+    const bg = router.query.tags && (router.query.tags as string).includes(tag) ? 'text-gray-100 bg-indigo-400 hover:bg-indigo-500' : 'text-gray-700 bg-gray-200 hover:bg-gray-400'
+
     return (
         <Link href={{ pathname: '/', query: { tags: tag } }} as={`/?tags=${tag}`} shallow={true} >
-            <a className={`inline-block px-3 py-1 ${(i < length) && 'mr-2'} my-1 text-sm font-semibold text-gray-700 hover:bg-gray-400 bg-gray-200 rounded-full`}>
+            <a className={`inline-block px-3 py-1 ${(i < length) && 'mr-2'} my-1 text-sm font-semibold ${bg} rounded-full transition-all duration-200 ease-in`}>
                 #{tag}
             </a>
         </Link>
