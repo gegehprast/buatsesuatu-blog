dff83604c29d57d9a0722b5de678bbd980d52ac2
gegehprast <gegeh.prast@gmail.com>
2020-07-01 14:35:37 +0800

    update markdown anchor for internal article to use next link

--- components/MarkdownRenderes/Anchor.tsx
+++ components/MarkdownRenderes/Anchor.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 
 interface Props {
     children: string
@@ -6,6 +7,16 @@ interface Props {
 }
 
 const Anchor = (props: Props): JSX.Element => {
+    if (props.href?.startsWith('https://buatsesuatu.dev/articles')) {
+        const slug = props.href.replace('https://buatsesuatu.dev/articles/', '')
+        
+        return <Link href="/articles/[slug]" as={`/articles/${slug}`}>
+            <a href={props.href} className="text-indigo-600 visited:text-purple-600 hover:text-indigo-800">
+                {props.children}
+            </a>
+        </Link>
+    }
+
     return <a href={props.href} className="text-indigo-600 visited:text-purple-600 hover:text-indigo-800" target="_blank" rel="noreferrer">
         {props.children}
     </a>
