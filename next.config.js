1a87ae56c4827472fab7322d78a0d1e75b4f9f99
gegehprast <gegeh.prast@gmail.com>
2021-09-10 15:48:53 +0700

    rewrites rules for real static page

--- next.config.js
+++ next.config.js
@@ -5,6 +5,18 @@ const nextConfig = {
     images: {
         domains: ['i.imgur.com'],
     },
+    rewrites: async () => {
+        return [
+            {
+                source: '/sesuatu/:customPage',
+                destination: '/sesuatu/:customPage/index.html',
+            },
+            {
+                source: '/sesuatu/:customPage/js/:jsFile',
+                destination: '/sesuatu/:customPage/js/:jsFile',
+            },
+        ]
+    }
 }
 
 // eslint-disable-next-line no-undef
